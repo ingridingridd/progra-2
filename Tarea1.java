18303b126d15060366c3ae3327a142b9c72a27f7
ingridingridd <114359454+ingridingridd@users.noreply.github.com>
2022-10-08 02:41:42 -0300

    Add files via upload

--- Tarea1.java
+++ Tarea1.java
@@ -1,5 +1,6 @@
 package tarea1;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 class Cliente{
@@ -47,12 +48,15 @@ class OrdenCompra{
 
 class DetalleOrden{
     private int cantidad;
+    private ArrayList<Articulo> detalle;
     
     public DetalleOrden(int c){
         cantidad = c;
+        detalle = new ArrayList();
     }
     
     public void calcularPrecio(){
+        //calcular precio total
         
     }
     public void calcularPrecioSinIva(){
@@ -72,11 +76,32 @@ class Articulo{
     private String descripcion;
     private float precio;  
     
-    public Articulo(){
-       
+    public Articulo(String nom, String desc, float peso, float p){
+       this.nombre = nom;
+       this.descripcion = desc;
+       this.precio = p;
+       this.peso = peso;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nuevoNom){
+        this.nombre = nuevoNom;
+    }
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+    public void setDescripcion(String nuevaDesc){
+        this.descripcion= nuevaDesc;
+    }
+    public Float getPrecio(){
+        return this.precio;
+    }
+    public void setPrecio(Float nuevoPrecio){
+        this.precio= nuevoPrecio;
     }
 }
-
+ 
 class Pago{
     private float monto;
     private Date fecha;
@@ -90,14 +115,22 @@ class Efectivo extends Pago{
     public Efectivo(){
         
     }
+    public void calcDevolucion(){
+        
+    }
 }
 class Transferencia extends Pago{
+    private String banco;
+    private String numCuenta;
     public Transferencia(){
         
     }
 }
 
 class Tarjeta extends Pago{
+    private String tipo;
+    private String numTransaccion;
+    
     public Tarjeta(){
         
     }
