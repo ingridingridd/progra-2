5d886a7c828f021029739f29dede45ba1de49fbe
ingridingridd <114359454+ingridingridd@users.noreply.github.com>
2022-10-09 18:49:16 -0300

    Add files via upload

--- Tarea1.java
+++ Tarea1.java
@@ -6,6 +6,7 @@ import java.util.Date;
 class Cliente{
     private String nombre;
     private String rut;
+    public Direccion direc;
     
     public Cliente(String nom, String r){
         nombre = nom;
@@ -17,12 +18,23 @@ class Cliente{
     public void setNombreCliente(String nuevoNomCliente){
         this.nombre = nuevoNomCliente;
     }
+     public String getRutCliente(){
+        return this.rut;
+    }
+    public void setRutCliente(String nuevoRutCliente){
+        this.nombre = nuevoRutCliente;
+    }
+    public String getDirCliente(){
+        return this.direc.getDireccion();
+    }
 }
 
 class Direccion{
     private String direccion;
+    public Cliente dirCliente;
+    public DocumentoTributario docTributario;
     
-    public Direccion( String direc){
+    public Direccion(String direc){
         direccion = direc;
     } 
     public String getDireccion(){
@@ -36,17 +48,18 @@ class Direccion{
 class OrdenCompra{
     private Date fecha;
     private String estado;
+    public DetalleOrden detOrden; //para asociar las clases
     
     public OrdenCompra(String est){
-        //constructor
       //  fecha = f;
         estado = est;
     }
+    
     public void calcularPrecioSinIva(){
        
     }
-    public float calcularIva(float precio){
-        float iva = precio*(19/100);
+    public double calcularIva(float precio){
+        double iva = precio*(1.19);
         return iva;
     }
     public void calcularPrecio(){
@@ -55,37 +68,47 @@ class OrdenCompra{
     public void calcularPeso(){
         
     }
+    public void addDet(int cant, Articulo a){
+        
+    }
+    
+    public String getEstado(){
+        return this.estado;
+    }
 }
 
 class DetalleOrden{
     private int cantidad;
     private ArrayList<Articulo> detalle;
+    public OrdenCompra ordenCompra;
+    public Articulo producto;
     
     public DetalleOrden(int c){
         cantidad = c;
         detalle = new ArrayList();
     }
     
-    public double calcularPrecio(Articulo art){
+    public double calcPrecio(Articulo art){
         //calcular precio total
         double calcularPrecio = (float)cantidad*art.getPrecio()*1.19;
 //        detalle.add(cantidad, art);
         return calcularPrecio;
     }
-    public double calcularPrecioSinIva(Articulo art){
+    public double calcPrecioSinIva(Articulo art){
 //        double iva = art.getPrecio()*0.19;
 //        return cantidad*art.getPrecio() - cantidad*iva;
           return cantidad*art.getPrecio();
     }
-    public double calcularIva(Articulo art){
+    public double calcIva(Articulo art){
         double iva= art.getPrecio()*0.19;
         return iva*cantidad;
     }
-    public float calcularPeso(Articulo art){
+    public float calcPeso(Articulo art){
         return art.getPeso()*cantidad;
     }
-    public void addDetalle(Articulo a){
-        detalle.add(a);
+    
+    public int getCantidad(){
+        return this.cantidad;
     }
 }
 
@@ -94,6 +117,7 @@ class Articulo{
     private String nombre;
     private String descripcion;
     private float precio;  
+    public DetalleOrden detOrden;
     
     public Articulo(String nom, String desc, float peso, float prec){
        this.nombre = nom;
@@ -130,13 +154,16 @@ class Articulo{
 class Pago{
     private float monto;
     private Date fecha;
-    private int tipoPago;
+
     
     public Pago(float mont){
         monto = mont;
     }
     public Float getMonto(){
         return this.monto;
+    }
+    public String metodoPago(){
+        return "Metodo Pago: ";
     }
 }
 
@@ -147,16 +174,24 @@ class Efectivo extends Pago{
     public float calcDevolucion(float pagado){
         //vuelto
         float total = super.getMonto();
-        return total-pagado;
+        return pagado-total;
     }
-    
+    public String metodoPago(){
+        return super.metodoPago()+ "Efectivo";
+    }
 }
 class Transferencia extends Pago{
     private String banco;
     private String numCuenta;
     
-    public Transferencia(int i){
+    public Transferencia(int i, String banc, String ncuenta){
         super(i);
+        banco = banc;
+        numCuenta= ncuenta;
+    }
+    
+    public String getBanco(){
+        return this.banco;
     }
 }
 
@@ -164,8 +199,8 @@ class Tarjeta extends Pago{
     private String tipo;
     private String numTransaccion;
     
-    public Tarjeta(){
-        
+    public Tarjeta(int i){
+        super(i);
     }
 }
 
@@ -173,15 +208,27 @@ class DocumentoTributario{
     private String numero;
     private String rut;
     private Date fecha;
+    public Direccion direc; 
     
-    public DocumentoTributario(){
-        
+    public DocumentoTributario(String num, String rutt){
+        numero = num;
+        rut = rutt;
+    }
+    
+    public String getNumero(){
+        return this.numero;
+    }
+    public String getRut(){
+        return this.rut;
+    }
+    public String getDirec(){
+        return this.direc.getDireccion();
     }
 }
 
 class Factura extends DocumentoTributario{
-    public Factura(int i){
- //       super(i);
+    public Factura(String numero, String rut){
+        super(numero, rut);
     }
 }
 
@@ -194,24 +241,105 @@ class Boleta extends DocumentoTributario{
 public class Tarea1 {
 
     public static void main(String[] args) {
-        OrdenCompra orden = new OrdenCompra("entregado");
-        DetalleOrden det = new DetalleOrden(10); 
-        Articulo art = new Articulo("chocochips", "con chips de chocolate", 200, 1000);
-        Articulo art1 = new Articulo("Rolls", "mani cubierto de chocolate", 150, 1500);
-        det.addDetalle(art);
-        det.addDetalle(art1);
-        Pago pa = new Pago(20000);
-        Efectivo e = new Efectivo(20000);
-
+        //Orden1
+        //cliente1
+        Cliente cliente1 = new Cliente("Selena Gomez", "11234567-8");
+        Direccion direc1 = new Direccion("Las violetas 1234");
+        cliente1.direc = direc1;
+        System.out.println(cliente1.getNombreCliente()+" " + cliente1.getRutCliente()+ " " + cliente1.getDirCliente());
         
-        System.out.println(art.getNombre());
-        System.out.println(art.getDescripcion());
-        System.out.println(det.calcularIva(art));
-        System.out.println(art1.getNombre());
+        //cliente2
+        Cliente cliente2 = new Cliente("Taylor Swift", "12345678-9");
+        Direccion direc2 = new Direccion("Los Lirios 189");
+        cliente2.direc = direc2;
+        
+        
+        OrdenCompra orden1 = new OrdenCompra("Pagado");
+        DetalleOrden det1 = new DetalleOrden(2);
+        DetalleOrden det2 = new DetalleOrden(5);
+        DetalleOrden det3 = new DetalleOrden(3);
+        Articulo art1 = new Articulo("milka", "chocolate con relleno de trufa", 150, 1600);
+        Articulo art2 = new Articulo("super8", "galleta cubierta de chocolate", 80, 300);
+        Articulo art3 = new Articulo("chocochips", "con chips de chocolate", 200, 1000);
+        Articulo art4 = new Articulo("chocman", "Bizcocho bañado de chocolate", 70, 300);
+        Articulo art5 = new Articulo("chocochips", "con chips de chocolate", 200, 1000);
+        Articulo art6 = new Articulo("Sprite", "bebida salor limon", 500, 1200);
+        
+        System.out.println("---Detalle compra---");
+        System.out.println("Cantidad: " + det1.getCantidad()+ " " +art1.getNombre() + " " + art1.getDescripcion() + " " + art1.getPeso()+ "gr " + art1.getPrecio()+" PRECIO:"+ det1.calcPrecioSinIva(art1)+" IVA:"+ det1.calcIva(art1)+" TOTAL:"+ det1.calcPrecio(art1));
+        System.out.println("Cantidad: " + det2.getCantidad()+" " + art2.getNombre() + " " + art2.getDescripcion() + " " + art2.getPeso()+ "gr " + art2.getPrecio()+" PRECIO:"+ det2.calcPrecioSinIva(art2)+" IVA:"+ det2.calcIva(art2)+" TOTAL:"+ det2.calcPrecio(art2));
+        System.out.println("Cantidad: " + det3.getCantidad()+" " +art4.getNombre() + " " + art4.getDescripcion() + " " + art4.getPeso()+ "gr " + art4.getPrecio()+" PRECIO:"+ det3.calcPrecioSinIva(art4)+" IVA:"+ det3.calcIva(art4)+" TOTAL:"+ det3.calcPrecio(art4));
+        
+        double total= det1.calcPrecio(art1)+ det2.calcPrecio(art2)+det3.calcPrecio(art4);
+        System.out.println("TOTAL: " + total);
+        System.out.println("Estado: " + orden1.getEstado());
+        
+        /*
+        ArrayList<Articulo> detalle1 = new ArrayList<>();
+            detalle1.add(new Articulo("chocochips", "con chips de chocolate", 200, 1000));
+            detalle1.add(new Articulo("Rolls", "mani cubierto de chocolate", 150, 1500));
+            detalle1.add(new Articulo("milka", "chocolate con relleno de trufa", 150, 1600));
+            detalle1.add(new Articulo("super8", "galleta cubierta de chocolate", 80, 300));
+            detalle1.add(new Articulo("chocman", "Bizcocho bañado de chocolate", 70, 300));
+         
+         System.out.println("---Detalle compra---");
+         System.out.println("Estado: " + orden1.getEstado());
+         
+        // System.out.println(art1.getNombre()+ ": " + art1.getDescripcion()+ " " + art1.getPrecio());
+         System.out.println(detalle1.get(0).getNombre() + " " +detalle1.get(0).getDescripcion()+ " " + detalle1.get(0).getPeso()+" gr " + " " +detalle1.get(0).getPrecio());
+         System.out.println(detalle1.get(1).getNombre() + " " +detalle1.get(1).getDescripcion()+ " " + detalle1.get(1).getPeso()+" gr " + " " +detalle1.get(1).getPrecio());
+         System.out.println(detalle1.get(2).getNombre() + " " +detalle1.get(2).getDescripcion()+ " " + detalle1.get(2).getPeso()+" gr " + " " +detalle1.get(2).getPrecio());
+         System.out.println(detalle1.get(3).getNombre() + " " +detalle1.get(3).getDescripcion()+ " " + detalle1.get(3).getPeso()+" gr " + " " +detalle1.get(3).getPrecio());
+         System.out.println(detalle1.get(4).getNombre() + " " +detalle1.get(4).getDescripcion()+ " " + detalle1.get(4).getPeso()+" gr " + " " +detalle1.get(4).getPrecio());
+         
+         System.out.println("IVA total: " + det1.calcIva(detalle1.get(0)));
+         System.out.println("IVA producto 2: " + det1.calcIva(detalle1.get(1)));
+        */ 
+         System.out.println("---Detalle Pago---");
+         Transferencia tr = new Transferencia(2, "Estado", "189023");
+         System.out.println("Banco: " + tr.getBanco());
+         
+        //"Tipo Documento:"
+         DocumentoTributario doc = new DocumentoTributario("123", "12345-6");
+         Factura factura = new Factura("13", "12345-6"); 
+         if(doc.getNumero().equals(factura.getNumero())){
+             System.out.println("Tipo Documento: Factura");
+         }
+         else System.out.println("Tipo Documento: Boleta");
+
+        Pago pa = new Pago(10000);
+        Efectivo e = new Efectivo(20000);
+        System.out.println("Tipo pago: ");
         System.out.println("vuelto: " + e.calcDevolucion(10000));
-        System.out.println("Peso total (gramos):" + det.calcularPeso(art));
-        System.out.println(det.calcularPrecioSinIva(art));
-        System.out.println(det.calcularPrecio(art));
+        
+        //orden2
+        System.out.println();
+        System.out.println(cliente2.getNombreCliente()+" " + cliente2.getRutCliente()+ " " + cliente2.getDirCliente());
+        System.out.println("---Detalle compra---");
+        
+        OrdenCompra orden2 = new OrdenCompra("Pagado");
+        DetalleOrden det4 = new DetalleOrden(1);
+        DetalleOrden det5 = new DetalleOrden(3);
+        
+        System.out.println("Cantidad: " + det4.getCantidad()+ " " +art6.getNombre() + " " + art6.getDescripcion() + " " + art6.getPeso()+ "gr " + art6.getPrecio()+" PRECIO:"+ det4.calcPrecioSinIva(art6)+" IVA:"+ det4.calcIva(art6)+" TOTAL:"+ det4.calcPrecio(art6));
+        System.out.println("Cantidad: " + det5.getCantidad()+" " + art5.getNombre() + " " + art5.getDescripcion() + " " + art5.getPeso()+ "gr " + art5.getPrecio()+" PRECIO:"+ det5.calcPrecioSinIva(art5)+" IVA:"+ det5.calcIva(art5)+" TOTAL:"+ det5.calcPrecio(art5));
+        
+        double total2 = det4.calcPrecio(art6)+ det5.calcPrecio(art5);
+        System.out.println("TOTAL: " + total2);
+        System.out.println("Estado: " + orden1.getEstado());
+        
+        //"Tipo Documento:"
+         DocumentoTributario docu2 = new DocumentoTributario("12", "1232845-6");
+         Factura factura2 = new Factura("13", "123405-6"); 
+         if(docu2.getNumero().equals(factura2.getNumero())){
+             System.out.println("Tipo Documento: Factura");
+         }
+         else System.out.println("Tipo Documento: Boleta");
+
+        Pago pa2 = new Pago(10000);
+        Efectivo e2 = new Efectivo(10000);
+        System.out.println("Tipo pago: ");
+        System.out.println("vuelto: " + e2.calcDevolucion(5000));
     }
     
 }
